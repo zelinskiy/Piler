8d446a1599e2990a6b652515e1f241e86f80aa64
zelinskiy <zelinskiy1917@gmail.com>
2017-12-15 16:10:22 +0000

    Done generic JSON

--- front/src/Types/Login.purs
+++ front/src/Types/Login.purs
@@ -2,18 +2,10 @@ module Types.Login where
 
 import Prelude
 
-import Data.Argonaut (class EncodeJson, class DecodeJson, decodeJson, encodeJson, (:=), jsonEmptyObject, (~>), (.?))
+import Data.Argonaut (class EncodeJson, class DecodeJson)
 import Data.Generic (class Generic)
-import Data.Maybe (fromMaybe)
-import Data.Either(Either(..), either)
-import Data.Array
 
---from purescript-maps
-import Data.StrMap as M
-
-import Data.Argonaut.Encode.Generic (gEncodeJson)
-import Data.Argonaut.Decode.Generic (gDecodeJson)
-import Data.Argonaut.Core(fromString, toObject, toArray, fromArray, fromObject, jNull, fromNull)
+import Utils.Other
 
 newtype Login = Login
                 { email :: String
@@ -22,46 +14,14 @@ newtype Login = Login
 defaultLogin :: Login
 defaultLogin = Login { email: "user1@mail.com"
                      , pass: "pass" }
-{-
+
+
 derive instance genericLogin :: Generic Login
 
 instance decodeJsonLogin :: DecodeJson Login where
-  decodeJson j = gDecodeJson j'
-    where
-      j' = fromObject
-           $ M.insert "tag" (fromString "Types.Login")
-           $ M.insert "values" (decodeJson j)
-           $ M.empty
-
--- $ SM.insert "tag" (fromString (reflectSymbol (SProxy :: SProxy name)))
+  decodeJson = genericDecodeNewtypeJson
 
 instance encodeJsonLogin :: EncodeJson Login where
-  encodeJson l = 
-    let d = gEncodeJson l
-    in fromMaybe (fromNull jNull) $ do
-      o <- toObject d
-      vs <- M.lookup "values" o
-      vs' <- toArray vs
-      head vs'
+  encodeJson = genericEncodeNewtypeJson
 
-wat :: String
-wat =
-  case decodeJson (encodeJson defaultLogin) of
-    Left e -> e
-    Right (Login l) -> l.email
--}
-
-
-instance decodeJsonLogin :: DecodeJson Login where
-  decodeJson json = do
-    o <- decodeJson json
-    email <- o .? "email"
-    pass <- o .? "pass"
-    pure $ Login { email, pass }
-
-instance encodeJsonLogin :: EncodeJson Login where
-  encodeJson (Login { email: e, pass: p })
-     = "email" := e
-    ~> "pass" := p
-    ~> jsonEmptyObject
 
--- front/src/Utils/Other.purs
+++ front/src/Utils/Other.purs
@@ -3,6 +3,29 @@ module Utils.Other where
 import Prelude
 
 import Data.String(take, drop, length)
+import Data.Either(Either)
 
+import Data.Argonaut.Generic.Argonaut(options)
+import Data.Argonaut.Generic.Options(Options(Options))
+import Data.Argonaut.Generic.Encode (genericEncodeJson)
+import Data.Argonaut.Generic.Decode (genericDecodeJson)
+import Data.Argonaut (Json)
+import Data.Generic (class Generic)
+                      
 trimAny :: String -> String
 trimAny = drop 1 <<< (\s -> take (length s - 1) s)
+
+genericNewtypeJsonOptions :: Options
+genericNewtypeJsonOptions = Options $ case options of
+  Options o -> o { encodeSingleConstructors = false
+                 , flattenContentsArray = true }
+
+genericDecodeNewtypeJson :: forall a. Generic a 
+                            => Json -> Either String a
+genericDecodeNewtypeJson =
+  genericDecodeJson genericNewtypeJsonOptions
+
+genericEncodeNewtypeJson :: forall a. Generic a
+                            => a -> Json
+genericEncodeNewtypeJson =
+  genericEncodeJson genericNewtypeJsonOptions
