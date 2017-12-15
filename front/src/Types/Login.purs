7c7b58df5c220c092cc5b8ed4eefc8b6154f96d7
zelinskiy <zelinskiy1917@gmail.com>
2017-12-15 13:01:57 +0200

    Non generic Json due to newtype tags

--- front/src/Types/Login.purs
+++ front/src/Types/Login.purs
@@ -1,10 +1,19 @@
 module Types.Login where
 
-import Data.Argonaut (class EncodeJson, class DecodeJson)
-import Data.Generic (class Generic)
+import Prelude
 
-import Data.Argonaut.Encode.Generic.Rep (gEncodeJson)
-import Data.Argonaut.Decode.Generic.Rep (gDecodeJson)
+import Data.Argonaut (class EncodeJson, class DecodeJson, decodeJson, encodeJson, (:=), jsonEmptyObject, (~>), (.?))
+import Data.Generic (class Generic)
+import Data.Maybe (fromMaybe)
+import Data.Either(Either(..), either)
+import Data.Array
+
+--from purescript-maps
+import Data.StrMap as M
+
+import Data.Argonaut.Encode.Generic (gEncodeJson)
+import Data.Argonaut.Decode.Generic (gDecodeJson)
+import Data.Argonaut.Core(fromString, toObject, toArray, fromArray, fromObject, jNull, fromNull)
 
 newtype Login = Login
                 { email :: String
@@ -17,13 +26,32 @@ defaultLogin = Login { email: "user1@mail.com"
 derive instance genericLogin :: Generic Login
 
 instance decodeJsonLogin :: DecodeJson Login where
-  decodeJson = gDecodeJson
+  decodeJson j = gDecodeJson j'
+    where
+      j' = fromObject
+           $ M.insert "tag" (fromString "Types.Login")
+           $ M.insert "values" (decodeJson j)
+           $ M.empty
 
+-- $ SM.insert "tag" (fromString (reflectSymbol (SProxy :: SProxy name)))
 
 instance encodeJsonLogin :: EncodeJson Login where
-  encodeJson = gEncodeJson
+  encodeJson l = 
+    let d = gEncodeJson l
+    in fromMaybe (fromNull jNull) $ do
+      o <- toObject d
+      vs <- M.lookup "values" o
+      vs' <- toArray vs
+      head vs'
+
+wat :: String
+wat =
+  case decodeJson (encodeJson defaultLogin) of
+    Left e -> e
+    Right (Login l) -> l.email
 -}
-{-
+
+
 instance decodeJsonLogin :: DecodeJson Login where
   decodeJson json = do
     o <- decodeJson json
@@ -36,4 +64,4 @@ instance encodeJsonLogin :: EncodeJson Login where
      = "email" := e
     ~> "pass" := p
     ~> jsonEmptyObject
--}
+
